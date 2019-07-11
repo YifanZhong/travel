17f8fb5e82eb579cc7ca966d59eaa690e3ad4f8e
Ivan Zhong <ivanzhong0310@gmail.com>
2019-07-12 02:11:31 +1200

    code check

--- src/main/java/cn/itcast/travel/web/servlet/RegistUserServlet.java
+++ src/main/java/cn/itcast/travel/web/servlet/RegistUserServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
@@ -19,6 +20,29 @@ import java.util.Map;
 @WebServlet(name = "/registUserServlet")
 public class RegistUserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //验证校验
+        String check = request.getParameter("check");
+        //从Session中获取验证码
+        HttpSession session = request.getSession();
+        String checkcode_server = (String) session.getAttribute("CHECKCODE_SERVER");
+        session.removeAttribute("HECKCODE_SERVER"); //为了保证验证码只能使用一次
+
+        //比较
+        if (checkcode_server == null || !checkcode_server.equalsIgnoreCase(check)){
+            //验证码错误
+            ResultInfo info = new ResultInfo();
+            // 注册失败
+            info.setFlag(false);
+            info.setErrorMsg("验证码错误");
+            //将resultInfo对象序列化成json
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(info);
+            response.setContentType("application/json;charset=utf-8");
+            response.getWriter().write(json);
+            return;
+        }
+
 
         //1.获取数据（因为已经提前处理编码了）
         Map<String, String[]> map = request.getParameterMap();
