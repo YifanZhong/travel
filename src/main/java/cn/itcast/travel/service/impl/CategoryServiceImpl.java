9bbb6260172d7630b125c2c811b124b81da9e289
Ivan Zhong <ivanzhong0310@gmail.com>
2019-07-15 18:22:31 +1200

    cache optimizing using redis

--- src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -4,8 +4,12 @@ import cn.itcast.travel.dao.CategoryDao;
 import cn.itcast.travel.dao.impl.CategoryDaoImpl;
 import cn.itcast.travel.domain.Category;
 import cn.itcast.travel.service.CategoryService;
+import cn.itcast.travel.util.JedisUtil;
+import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class CategoryServiceImpl implements CategoryService {
 
@@ -13,6 +17,40 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> findAll() {
-        return categoryDao.findAll();
+        //1.从redis中查询
+        //1.1获取jedis客户端
+        Jedis jedis = JedisUtil.getJedis();
+        //1.2可使用sortedset排序查询
+        Set<String> categorys = jedis.zrange("category", 0, -1);
+
+        List<Category> cs = null;
+        //2.判断查询的集合是否为空
+        if (categorys == null || categorys.size() == 0 ) {
+            //3.如果为空，第一次访问，需要从数据库查询，在将数据存入redis
+            //3.1 从数据库查询
+            cs = categoryDao.findAll();
+            //3.2 将集合数据存储到Redis中的 category的key的sortedset中
+            for (int i = 0; i < cs.size(); i++) {
+                jedis.zadd("category", cs.get(i).getCid(), cs.get(i).getCname());
+            }
+
+
+        }else {
+            //4.如果不为空，直接返回
+
+            //转换
+            cs = new ArrayList<Category>();
+            for (String name : categorys) {
+                Category category = new Category();
+                category.setCname(name);
+                cs.add(category);
+
+            }
+
+
+
+        }
+        
+        return cs;
     }
 }
